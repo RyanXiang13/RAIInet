9ac68af09b7226428206edb8f54eac2fbf884c02
RyanXiang13 <xiangryan11@gmail.com>
2024-11-25 00:26:17 -0500

    implementation of ability class

---
+++ ability.cc
@@ -0,0 +1,8 @@
+#include <memory>
+#include "link.h"
+#include "cell.h"
+#include "player.h"
+#include "ability.h"
+
+// no implementation needed for pure virtual use
+// no implementation needed for destructor
