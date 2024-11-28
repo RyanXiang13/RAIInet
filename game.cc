f64a8b0987512f7d93e7c869238a24b9240dda24
Altaz P <altazp@gmail.com>
2024-11-28 00:34:56 -0500

    Get textobserver working

--- game.cc
+++ game.cc
@@ -3,23 +3,25 @@
 #include "cell.h"
 #include <vector>
 #include <memory>
+#include <iostream>
 
 Game::Game(std::unique_ptr<Player> p1, std::unique_ptr<Player> p2) {
   players.emplace_back(std::move(p1));
   players.emplace_back(std::move(p2));
   board.resize(height);
   for (int i = 0; i < height; ++i) {
-    board.resize(width);
+    board[i].resize(width);
     for (int j = 0; j < width; ++j) {
       board[i][j] = Cell::create(i, j);
     }
   }
-  const auto& p1Links = p1->getOwnedLinks();
+
+  auto& p1Links = players[0]->getOwnedLinks();
   for (const auto& link : p1Links) {
-    board[link->getRow()][link->getCol()]->setLink(link.get());
+  board[link->getRow()][link->getCol()]->setLink(link.get());
   }
 
-  const auto& p2Links = p2->getOwnedLinks();
+  const auto& p2Links = players[1]->getOwnedLinks();
   for (const auto& link : p2Links) {
     board[link->getRow()][link->getCol()]->setLink(link.get());
   }
@@ -28,11 +30,10 @@ Game::Game(std::unique_ptr<Player> p1, std::unique_ptr<Player> p2) {
   board[0][4]->setIsServerPort(1);
   board[7][3]->setIsServerPort(2);
   board[7][4]->setIsServerPort(2);
-
 }
 
 std::unique_ptr<Player>& Game::getPlayer(int index) {
-  return players[index - 1];
+  return players[index];
 }
 
 char Game::charAt(int i, int j) {
